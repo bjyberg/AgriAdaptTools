346eadf950147af8375c2aeec35ff804c5e41395
bjyberg <bjyberg1@gmail.com>
2023-11-24 17:34:52 +0000

    fix: :bug: Fixes to pass devtools check
    mostly fixes of documentation, but minor cod changes in explore_s3 to pass checks.

--- R/explore_s3.r
+++ R/explore_s3.r
@@ -1,22 +1,53 @@
-library(aws.s3)
-library(jsonlite)
-library(data.tree)
-library(s3fs)
-
+#' Explore S3 Bucket
+#'
+#' This function is used to explore the contents of a S3 bucket. It is designed to return an object or viewer that
+#' is as clos to a normal directory tree as possible. It can also be used to return a dataframe that contiains the 
+#' data and structure of the bucket. 
+#' 
+#' opens a list of AWS S3 objects in a new window that is in the format of a collapsible directory tree shape
+#'
+#' @param bucket.path The path of the S3 bucket to explore. If not provided, a default path to the "Adaptation Atlas" data will be used
+#' @param public Logical value indicating whether the bucket is public or not. If the bucket it public, it prevents the use of AWS credentials avoiding an error.
+#' @param region The AWS region where the bucket is located. Default is "us-east-1".
+#' @param recursive Logical value indicating whether to explore the bucket recursively or not. Default is TRUE.
+#' @param return_type The format in which the information should be returned. Possible values are "list", "json", "data.frame", "tree". Default is "list".
+#' @param open.view Logical value indicating whether to open the list in a new window or not. Default is TRUE.
+#' 
+#' @return Depending on the specified return type, the function returns a list, hierarchical JSON, data frame, or tree object.
+#'
+#' @examples
+#' explore_s3(bucket.path = "noaa-gsod-pds", public = TRUE, recursive = FALSE, open.view = FALSE)
+#' 
+#' \dontrun{
+#'  Sys.setenv(
+#'     AWS_ACCESS_KEY_ID = "your AWS access key",
+#'     AWS_SECRET_ACCESS_KEY = "your AWS secret key",
+#'     AWS_DEFAULT_REGION = "your AWS region" (optional),
+#'     OR
+#'     AWS_CONFIG_FILE = "~/.aws/config" (or ~/path/to/aws/config/file),
+#'  )
+#' explore_s3() # this will default to the Adaptation Atlas Data
+#' }
+#' @importFrom s3fs S3FileSystem
+#' @importFrom data.tree as.Node
+#' @importFrom jsonlite toJSON
+#' @importFrom utils View
+#'
+#' @export
 explore_s3 <- function(
     bucket.path = NULL,
     public = FALSE,
     region = "us-east-1",
     recursive = TRUE,
+    open.view = TRUE,
     return_type = c("list", "json", "data.frame", "tree")) {
   return_type <- match.arg(return_type)
-  print(return_type)
   if (public) {
-    s3 <- S3FileSystem$new(region_name = region, anonymous = T)
+    s3 <- s3fs::S3FileSystem$new(region_name = region, anonymous = T)
   } else {
     tryCatch(
       {
-        s3 <- S3FileSystem$new(region_name = region)
+        s3 <- s3fs::S3FileSystem$new(region_name = region)
       },
       error = function(e) {
         stop("Could not connect to S3. Check your AWS credentials and region")
@@ -29,9 +60,11 @@ explore_s3 <- function(
   file_df$pathString <- paste0(file_df$bucket, "/", file_df$key)
   file_df$size_mb <- file_df$size / 1000000
   tree_df <- file_df[c("pathString", "size_mb", "last_modified", "uri")]
-  tree <- as.Node(tree_df)
+  tree <- data.tree::as.Node(tree_df)
   tree_list <- as.list(tree)
-  View(tree_list)
+  if (open.view) {
+    utils::View(tree_list)
+  }
   if (return_type == "json") {
     tree_json <- jsonlite::toJSON(tree_list, pretty = TRUE, na = "null")
     return(tree_json)
@@ -47,17 +80,10 @@ explore_s3 <- function(
   }
 }
 
-explore_s3()
-explore_s3("herbariumnsw-pds", public = TRUE, region = "ap-southeast-2", recursive = FALSE)
-
-
-
-
-
+# old function based oon the now abandoned aws.s3 library
+# explore_s3 <- function(s3.bucket, root, region = "us-east-1") {
 #   file_df <- aws.s3::get_bucket_df(s3.bucket, max = Inf,
 #     prefix = root, region = region)
-#    file_df <- aws.s3::get_bucket_df("digital-atlas", max = Inf,
-#      prefix = "atlas-data")
 #   file_df$pathString <- file_df$Key
 #   file_df$size_mb <- as.numeric(file_df$Size) / 1000000
 #   tree_df <- file_df[c("pathString", "size_mb", "LastModified")]
@@ -69,32 +95,3 @@ explore_s3("herbariumnsw-pds", public = TRUE, region = "ap-southeast-2", recursi
 #   View(tree_list)
 #   return(tree_list)
 # }
-
-# library(paws)
-# paws::
-# s3 <- paws::s3(list(region = "us-east-1"))
-# s3_data <- s3$list_objects_v2(Bucket = 'digital-atlas', Prefix = 'atlas-data')
-# s3_data <- s3_data[["Contents"]]
-# s3_data
-# explore_s3(s3.bucket = "s3://herbariumnsw-pds/", region = "ap-southeast-2")
-# herbarium_files <- get_bucket_df(
-#   bucket = "herbariumnsw-pds",
-#   region = "ap-southeast-2",
-#   max = 20000
-# )
-
-# library(s3fs)
-
-# herb <- S3FileSystem$new(region_name = "ap-southeast-2", anonymous = T)
-# s3_dir_info("digital-atlas/atlas-data")
-
-# herb$dir_info("herbariumnsw-pds") 
-# file_df <- as.data.frame(herb$dir_info("herbariumnsw-pds"))
-# file_df$pathString <- paste0("herbariumnsw-pds/", file_df$key)
-# data.tree::as.Node(file_df)
-
-# file_df$size_mb <- file_df$size / 1000000
-# tree_df <- file_df[c("pathString", "size_mb", "last_modified")]
-# tree <- as.Node(tree_df)
-# tree_list <- as.list(tree)
-# View(tree_list)
