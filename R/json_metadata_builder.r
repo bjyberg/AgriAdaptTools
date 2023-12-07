a19a9d994c9f82024137d3fa207c23c94dbd53dc
bjyberg <bjyberg1@gmail.com>
2023-12-07 19:18:06 +0000

    feat: :sparkles: The JSON metadata builder should be mostly there
    Changed the way definitiosn are added, cleaning for the auto identification of items that need defined, etc. Also linked up the collection and itemgroup metadat. collection first, itemgroup next

--- R/json_metadata_builder.r
+++ R/json_metadata_builder.r
@@ -1,115 +1,4 @@
-# library(terra)
-# library(jsonlite)
-
-# json_folder_metadata <- function(
-#     folder.name,
-#     folder.description,
-#     folder.source,
-#     data.author.name,
-#     data.author.email,
-#     metadata.author = data.author.name[1],
-#     folder.license = "CC-BY-4.0") {}
-#   if (length(author.name) != length(author.email)) {
-#     length(author.email) <- length(author.name)
-#   }
-#   author_df <- data.frame(
-#     name = author.name,
-#     email = author.email
-#   )
-#   metadata_df <- data.frame(
-#     metadataCreated = format(Sys.time(), "%Y-%m-%d"),
-#     metadataModified = format(Sys.time(), "%Y-%m-%d"),
-#     metadataAuthor = metadata.author
-#   )
-#   folder.source <- data.frame(
-#     license = folder.license,
-#     citation = folder.citation,
-#     sourceURL = folder.url,
-#     sourceDOI = folder.doi
-#   )
-
-
-
-
-#     data.frame(
-#         name = folder.name,
-#         description = folder.description,
-#         authors = author_df
-#     )
-
-
-
-# .files <- function(file_list) {
-# basename(file_list)
-# }
-
-# .coverage <- function(x) {
-#     coverage_df <- data.frame(holder = NA)
-#     coverage_df$crs <- data.frame(
-#       epsg = paste0(
-#         terra::crs(x, describe = T)[c("authority", "code")],
-#         collapse = ":"
-#       ),
-#       proj = terra::crs(x, proj = T),
-#       resolution = terra::res(x)[1],
-#       resolutionUnit = "decimal-degrees"
-#     )
-#     coverage_df$extent <- data.frame(
-#       row.names = NULL,
-#       xmin = terra::ext(x)$xmin,
-#       xmax = terra::ext(x)$xmax,
-#       ymin = terra::ext(x)$ymin,
-#       ymax = terra::ext(x)$ymax
-#     )
-#     coverage_df$temporal <- data.frame(
-#       start_date = "2016/01/01",
-#       end_date = "2018/12/31"
-#     )
-#     coverage_df <- coverage_df[-1]
-# }
-
-
-
-# json_item_metadata <- function(
-#     files,
-#     group.name,
-#     group.description,
-#     file.type,
-#     attributes,
-#     coverage.temporal.start_date,
-#     coverage.temporal.end_date,
-#     coverage.region,
-#     naming.format,
-#     naming.separator = "_"
-# ) {
-#     cl_format <- gsub("\\[|]", "", naming.format)
-#     name.split <- unlist(strsplit(cl_format, naming.separator))
-
-#     # for (i in name.split) {
-#     #     name =
-#     #     description =
-#     # }
-
-
-#     naming_df <- data.frame(
-#     naming.format,
-#     naming.separator
-
-
-#     )
-#     if (is.null(file.type)) {
-#         file.type <- unique(tools::file_ext(files))
-#     }
-#     data.frame(
-#         name = group.name,
-#         description = group.description,
-#         fileType = file.type,
-#         files = .files(files),
-
-#     )
-# }
-
-folder_metadata <- function(
+collection_metadata <- function(
     folder.path,
     folder.description,
     metadata.author,
@@ -122,7 +11,7 @@ folder_metadata <- function(
     source.citation = NULL,
     source.url = NULL,
     source.doi = NULL,
-    additional.assets = TRUE,
+    add.assets = TRUE,
     process.description = NULL,
     process.derived_from = NULL,
     process.code = NULL
@@ -131,7 +20,7 @@ folder_metadata <- function(
   meta.dateCreated <- format(Sys.time(), "%Y-%m-%d")
   meta.dateModified <- format(Sys.time(), "%Y-%m-%d")
   folder.source <- .source(source.license, source.citation, source.url, source.doi)
-  if (additional.assets) {
+  if (add.assets) {
     assets <- .assets()
   } else {
     assets <- NULL
@@ -165,12 +54,13 @@ folder_metadata <- function(
   return(folder.metadata)
 }
 
-add_group_metadata <- function(
+itemgroup_metadata <- function(
   folder_metadata,
   file_list, # NOTE: File List should be relative to top-level json folder metadata
   name,
-  description, #What is it /how is it different from th other groups?
+  description, #What is it /how is it different from the other groups?
   layers_fields, # should be the same across groups
+  file.type,
   author.name,
   author.email,
   source.license,
@@ -180,7 +70,6 @@ add_group_metadata <- function(
   group.unit,
   name.format,
   name.separator = "_",
-  name.definitions = TRUE, # opens an interactive name builder, highly encouraged to define aspects of the file names
   coverage.region,
   temporal.resolution,
   temporal.start_date,
@@ -188,7 +77,8 @@ add_group_metadata <- function(
   process.derived_from = NULL,
   process.description = NULL,
   process.code = NULL,
-  additional.assets = TRUE # open an interactive asset builder
+  add.definitions = TRUE, # opens an interactive name builder, highly encouraged to define aspects of the file names
+  add.assets = TRUE # open an interactive asset builder
 ){
   if (inherits(folder_metadata, "character")) {
     if (!file.exists(folder_metadata)) {
@@ -206,7 +96,7 @@ add_group_metadata <- function(
     ))
   }
   folder_metadata$dateModified <- format(Sys.time(), "%Y-%m-%d")
-  base_folder <- folder_metadata$name
+  base_folder <- folder_metadata$folderName
 
   if (!is.null(folder_metadata$source$authors)) {
     print("author provide in folder metadata. Setting group authors to NULL")
@@ -241,13 +131,12 @@ add_group_metadata <- function(
     print("code provided in folder metadata. Setting group code to NULL")
     process.code <- NULL
   }
-
   author <- .author(author.name, author.email)
   spatial <- .spatial_coverage(file_list[1], coverage.region)
   temporal <- .temporal_coverage(temporal.resolution, temporal.start_date, temporal.end_date)
   coverage <- c(spatial, temporal)
   source <- .source(source.license, source.citation, source.url, source.doi)
-  if (additional.assets) {
+  if (add.assets) {
     assets <- .assets()
   } else {
     assets <- NULL
@@ -255,56 +144,59 @@ add_group_metadata <- function(
   if (!exists("group.unit")) {
     group.unit <- NULL
   }
-  nameScheme <- .naming_metadata( # TODO: This needs to be fed the basename(file_list)
-    name.format,
-    name.separator = "_",
-    name.definitions,
-    basename(file_list),
-    group.unit)
-  
   files <- .files(file_list, base_folder)
-  # layers <- .layers_fields(layers_fields) #TODO: add a layer interactive that inherits from the group
+  test_files <<- files
+  nameScheme <- list(nameFormat = name.format, separator = name.separator)
+
+  if (add.definitions) {
+    definitions <- .sub_group_metadata(name.format,
+      name.separator,
+      files,
+      group.unit)
+    def_level <- menu(c("item group", "full collection"),
+      title = "Do the name definitions apply to the item group or full collection?")
+    if (def_level == 1) {
+      group.definitions <- definitions
+    }
+    if (def_level == 2) {
+      group.definitions <- NULL
+      folder_metadata$definitions <- append(folder_metadata$definitions, definitions)
+    }
+  } else {
+    group.definitions <- NULL
+  }
+
 
   group_metadata <- list(
     name = name,
     description = description,
     author = author,
     layers = layers_fields,
+    fileType = file.type,
     source = source,
+    coverage = coverage,
     assets = assets,
     process = list(
       derived_from = process.derived_from,
       description = process.description,
       code = process.code
     ),
-    coverage = coverage,
     naming = nameScheme,
+    definitions = group.definitions,
     files = files
   )
   
-  folder_metadata[["fileGroups"]] <- append(folder_metadata[["fileGroups"]], setNames(list(hold = group_metadata), name))
+  folder_metadata[["fileGroups"]] <- append(folder_metadata[["fileGroups"]],
+    setNames(list(hold = group_metadata), name))
 
   return(folder_metadata)
 }
 
-.naming_metadata <- function(
-  name.format,
-  name.separator = "_",
-  name.definitions,
-  file_list,
-  group.unit) {
-  if (name.definitions) {
-    sub_groups <- .sub_group_metadata(name.format, name.separator, file_list, group.unit)
-  } else {
-    sub_groups <- NULL
-  }
-  names <- list(nameFormat = name.format, separator = name.separator)
-  names <- append(names, sub_groups)
-}
-
 .sub_group_metadata <- function(name.format, name.separator, file_list, group.unit) {
   cat(paste("Now enter information for the parts of:", name.format, "\n"))
+  name.separator <- paste0(name.separator, "[", "]", collapse = "|")
   pieces <- unlist(strsplit(name.format, name.separator))
+  pieces <- gsub("\\[|\\]", "", pieces)
   name_parts <- list()
   for (i in pieces) {
     def <- readline(prompt = paste0("Enter definition for ", i, ": "))
@@ -317,9 +209,17 @@ add_group_metadata <- function(
       if (inherit == 1) {
         position <- match(i, pieces)
         vars <- lapply(strsplit(file_list, name.separator), `[`, position)
+        external_splits <<- strsplit(file_list, name.separator)
+        external_test <<- vars
         unique_vars <- unique(unlist(vars))
         for (uv in unique_vars) {
-          include <- menu(c("Yes", "No"), title = paste0("Include: ", uv))
+          if (is.na(uv) | uv == "" | is.null(uv)) {
+            include <- 2
+          } else {
+            uv <- gsub("[^[:alnum:][:space:]]", " ", uv)
+            uv <- trimws(uv)
+            include <- menu(c("Yes", "No"), title = paste0("Include: ", uv))
+          }
           if (include == 1) {
             repeat {
               def <- readline(prompt = paste0("Enter definition for ", uv, ": "))
@@ -378,12 +278,32 @@ add_group_metadata <- function(
       name_parts[[i]] <- list(definition = def)
     }
   }
+  add_more <- menu(c("Yes", "No"),
+    title = "Do you want to add another definition?")
+  if (add_more == 1) {
+    repeat{
+      name <- readline(prompt = "Enter name: ")
+      def <- readline(prompt = paste("Enter definition for ", name, ": "))
+      add_unit <- menu(c("Yes", "No"), title = paste0("Is ", name,
+        " associated with a unit that differs from other sub-variables?"))
+      if (add_unit == 1) {
+        unit <- readline(prompt = paste0("Enter unit for ", name, ": "))
+      } else {
+        unit <- NULL
+      }
+      name_parts[[name]] <- list(definition = def, unit = unit)
+    add_another <- menu(c("Yes", "No"),
+      title = "Do you want to add another definition?")
+      if (add_another == 2) {
+        break
+      }
+    }
+  }
   return(name_parts)
 }
 
 .files <- function(file_list, base_path) {
-  # rel_path <- fs::path_rel(file_list, base_path)
-  basename(file_list)
+  gsub(paste0(".*", base_path), "", file_list)
 }
 
 .processing_metadata <- function(process.derived_from,
@@ -440,12 +360,6 @@ add_group_metadata <- function(
   dict[[key]] <- value
   return(dict)
 }
-# tests for the dict
-# .dict("code_scripts", list("script1", "script2", "hgithghgh.com"))
-# .dict("code_scripts", list(.dict("script1", "this script does this"), "script2", "hgithghgh.com"))
-# .dict("code_scripts", list(list("script1", "this script does this"), list("script2", "hgithghgh.com")))
-# test <- .dict("code_scripts", list(list(script = "script1", definition = "this script does this"), list(script = "script2", definition = "hgithghgh.com")))
-# jsonlite::toJSON(test, pretty = T)
 
 .assets <- function() { #TODO: add custom fields?
   add_asset <- menu(c("Yes", "No"),
@@ -523,8 +437,8 @@ add_group_metadata <- function(
   return(coverage)
 }
 
-
-cmip_timeseries <- folder_metadata(
+# Testing scripts
+cmip_timeseries <- collection_metadata(
   folder.path = "/home/bjyberg/aws_bucket/hazards/cmip6_indices_seasonal/year_timeseries/",
   folder.description = "CMIP6 Year Hazard Indices",
   metadata.author = "Brayden Youngberg",
@@ -536,19 +450,19 @@ cmip_timeseries <- folder_metadata(
   source.citation = "Ramirez-Villegas, J., Achicanoy, H., Thornton, P.K. 2023. CMIP6 climate hazards: human heat stress index. CGIAR. Dataset.",
   source.url = NULL,
   source.doi = NULL,
-  additional.assets = FALSE,
+  add.assets = FALSE,
   process.description = NULL,
   process.derived_from = NULL,
   process.code = NULL)
 
-test <- add_group_metadata( # TODO: Full interactive version
+test <- itemgroup_metadata( # TODO: Full interactive version
   cmip_timeseries,
   file_list = list.files("/home/bjyberg/aws_bucket/hazards/cmip6_indices_seasonal/year_timeseries/", pattern = ".tif", full.names = TRUE),
-  name = "yearly hazard indices",
+  name = "Yearly Hazard Indices",
   description = "Hazard Indices",
-  # file.type = "COG",
+  file.type = "COG",
   layers_fields = "years",
-  author.name = "Peter Stewart",
+  author.name = "Stewart, P.",
   author.email = "p.stewart@cgiar.org",
   source.license = "open_source",
   source.citation = NULL,
@@ -557,7 +471,6 @@ test <- add_group_metadata( # TODO: Full interactive version
   group.unit = NULL,
   name.format = '[historical]/[scenario_model_time-period]_variable_summariseFN',
   name.separator = "_",
-  name.definitions = FALSE,
   coverage.region = "SSA",
   temporal.resolution = 'yearly',
   temporal.start_date = 1995,
@@ -565,110 +478,11 @@ test <- add_group_metadata( # TODO: Full interactive version
   process.derived_from = "CMIP6 data",
   process.description = "we built htiis thig",
   process.code = "conflict_layer-builder.r",
-  additional.assets = FALSE
+  add.assets = FALSE,
+  add.definitions = TRUE
 )
 
 
 folder_metadata |> jsonlite::toJSON(pretty = T) |> cat()
 
 View(test)
-
-test2 <- add_group_metadata( # TODO: Full interactive version
-  test,
-  file_list = list.files("/home/bjyberg/aws_bucket/hazards/cmip6_indices_seasonal/year_timeseries/", pattern = ".tif", full.names = TRUE),
-  name = "nongngs",
-  description = "Haggg",
-  # file.type = "COG",
-  layers_fields = "years",
-  author.name = "Peter Stewart",
-  author.email = "p.stewart@cgiar.org",
-  source.license = "open_source",
-  source.citation = NULL,
-  source.url = NULL,
-  source.doi = NULL,
-  group.unit = NULL,
-  name.format = "[historical]/[scenario_model_time-period]_variable_summariseFN",
-  name.separator = "_",
-  name.definitions = FALSE,
-  coverage.region = "SSA",
-  temporal.resolution = "yearly",
-  temporal.start_date = 1995,
-  temporal.end_date = 2080,
-  process.derived_from = "CMIP6 data",
-  process.description = "we built htiis thig",
-  process.code = "conflict_layer-builder.r",
-  additional.assets = FALSE
-)
-
-View(test2)
-
-test2 |> jsonlite::toJSON(pretty = T, auto_unbox = T)
-
-
-
-
-
-
-
-
-# coverage fn tests and join
-# sp <- .spatial_coverage(paste0("/vsis3/digital-atlas/hazards/cmip6_indices_seasonal/year_timeseries/",
-#   haxza_meta$fileGroups[[2]]$files[[1]]))
-# tmp <- .temporal_coverage()
-# coverage <- c(sp, tmp)
-
-# ### group test ------
-test <- add_group_metadata( # TODO: Full interactive version
-cmip_timeseries,
-file_list = "/home/bjyberg/Biodiversity_International/Adaptation_Atlas/Conflict/conflict_historical_2020-2023.tif",
-name = "conflict",
-description = "Historical Conflict",
-layers_fields = "conflict_historical_2020-2023.tif",
-author.name = c("young, b.", "stew, p."),
-author.email = "bjyberg@gmail",
-source.license = "open_source",
-source.citation = NULL,
-source.url = NULL, # TODO: fill nulls in the individual functions or mass !exists()
-source.doi = NULL,
-group.unit = "num",
-name.format = "variable_period_time-range",
-name.separator = "_",
-name.definitions = FALSE,
-coverage.region = "SSA",
-temporal.resolution = NA,
-temporal.start_date = 2020,
-temporal.end_date = 2023,
-process.derived_from = "ACled.xsls",
-process.description = "we built htiis thig",
-process.code = "conflict_layer-builder.r",
-additional.assets = FALSE
-)
-1
-technical_report # TODO: FORCE/suggest asset types?
-how to.dox
-~/howto.dox
-how to do the script
-1
-2
-the ac variable
-1
-1
-rando
-1
-unsure
-2
-time
-2
-
-test |> View()
-test |> jsonlite::toJSON(pretty = T)
-
-test2 <- test
-test2$name <- "other"
-test3 <- test
-test3$name <- "rando"
-folder_metadata[["fileGroups"]] <- list(conflict = test, other = test2)
-folder_metadata |> jsonlite::toJSON(pretty = T, auto_unbox = T)
-View(folder_metadata)
-
-folder_metadata[["fileGroups"]] <- append(folder_metadata[["fileGroups"]], list(rando = test3))
